b8f484c7bb0dcf5eb678d5ef0fd63684667baebd
Caleb Eby <caleb.eby01@gmail.com>
2017-07-11 10:36:09 -0700

    Fix file path

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ go build ./nplh.go
 mkdir build
 mv nplh build
 ls build
-curl --request POST --header "PRIVATE-TOKEN: $APIKEY" --form "file=nplh" https://gitlab.com/api/v3/projects/nplh%2Fnplh/uploads
+curl --request POST --header "PRIVATE-TOKEN: $APIKEY" --form "file=@build/nplh" https://gitlab.com/api/v3/projects/nplh%2Fnplh/uploads
