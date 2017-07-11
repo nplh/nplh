19029e0e84b68f7fa267ab555fb693590d5d88f7
Caleb Eby <caleb.eby01@gmail.com>
2017-07-11 16:58:25 -0700

    0.0.15

--- nplh.go
+++ nplh.go
@@ -75,7 +75,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "No Place Like Home"
 	app.Usage = "A quick dotfile linker"
-	app.Version = "0.0.14"
+	app.Version = "0.0.15"
 
 	app.Action = func(c *cli.Context) {
 		cli.ShowAppHelp(c)
