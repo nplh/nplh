bf3d98d8e8f692c125e62ead808fa555b5891124
Caleb Eby <caleb.eby01@gmail.com>
2017-07-11 10:03:03 -0700

    Upload binary

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ go build ./nplh.go
 mkdir build
 mv nplh build
 ls build
+curl --request POST --header "PRIVATE-TOKEN: $APIKEY" --form "file=nplh" https://gitlab.example.com/api/v3/projects/nplh%2Fnplh/uploads
