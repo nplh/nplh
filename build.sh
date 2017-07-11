ffe17a73366e31ad7fd4831bf0e945d5625d80fd
Caleb Eby <caleb.eby01@gmail.com>
2017-07-11 11:09:31 -0700

    0.0.4

--- build.sh
+++ build.sh
@@ -1,6 +1,21 @@
+#!/usr/bin/env bash
+
 go get
 go build ./nplh.go
 mkdir build
 mv nplh build
-ls build
-curl --request POST --header "PRIVATE-TOKEN: $APIKEY" --form "file=@build/nplh" https://gitlab.com/api/v3/projects/nplh%2Fnplh/uploads
+
+if git describe --exact-match HEAD; then
+  echo "Uploading bin"
+  release_binary="$(curl \
+    --request POST \
+    --header "PRIVATE-TOKEN: $APIKEY" \
+    --form "file=@build/nplh" \
+    https://gitlab.com/api/v3/projects/nplh%2Fnplh/uploads | \
+    jq -r '.markdown')"
+
+  echo $release_binary
+  echo $release_binary
+else
+  echo "Not a tag; not uploading"
+fi
