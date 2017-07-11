44c66608d6ab6636ba9f824b415be94e524f1177
Caleb Eby <caleb.eby01@gmail.com>
2017-07-11 10:08:57 -0700

    Switch gitlab url

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
-# apk update
-# apk upgrade
-# apk add git curl
 go get
 go build ./nplh.go
 mkdir build
 mv nplh build
 ls build
-curl --request POST --header "PRIVATE-TOKEN: $APIKEY" --form "file=nplh" https://gitlab.example.com/api/v3/projects/nplh%2Fnplh/uploads
+curl --request POST --header "PRIVATE-TOKEN: $APIKEY" --form "file=nplh" https://gitlab.com/api/v3/projects/nplh%2Fnplh/uploads
