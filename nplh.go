a14edb227a9febb249677c5d7907e0f255e8237c
Caleb Eby <caleb.eby01@gmail.com>
2017-07-11 11:41:23 -0700

    Fix quotes

--- nplh.go
+++ nplh.go
@@ -75,7 +75,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "No Place Like Home"
 	app.Usage = "A quick dotfile linker"
-	app.Version = "0.0.8"
+	app.Version = "0.0.9"
 
 	app.Action = func(c *cli.Context) {
 		cli.ShowAppHelp(c)
