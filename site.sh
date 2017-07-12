4515001c1fccedfdcfb5e5673de4ea1d97a835d3
Caleb Eby <caleb.eby01@gmail.com>
2017-07-11 21:57:07 -0700

    Don't use jq for now

--- site.sh
+++ site.sh
@@ -5,18 +5,25 @@ mkdir build
 
 redirects=""
 
-jq --version || apt-get -y install jq
-
 redirect() {
   redirects=$redirects$1"   "$2$'\n'
 }
 
 latest=$(git describe --abbrev=0)
+# latest_url="$(curl \
+#   https://gitlab.com/api/v3/projects/nplh%2Fnplh/repository/tags/$latest | \
+#   jq -r ".release.description")"
+
 latest_url="$(curl \
   https://gitlab.com/api/v3/projects/nplh%2Fnplh/repository/tags/$latest | \
-  jq -r ".release.description")"
+  sed -r 's/\{|,/\n/g' | \
+  grep description | \
+  sed -r 's/:/\n/' | \
+  tail -n 1 | \
+  cut -d \" -f2
+)"
 
-redirect /dl $latest_url
+redirect /dl "$latest_url"
 
 echo "$redirects"
 echo "$redirects" > build/_redirects
