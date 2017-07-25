82cc53965e2601fb0f89ac3cd405d4b4ec516176
Caleb Eby <caleb.eby01@gmail.com>
2017-07-25 16:58:45 -0700

    Only release if tests pass

--- release.sh
+++ release.sh
@@ -1,10 +1,14 @@
-old_version=$(curl -s https://api.github.com/repos/nplh/nplh/releases/latest | jq -r ".tag_name")
-echo "New Version: (currently $old_version)"
-read version
-sed -i "s/$old_version/$version/g" *.*
+if go test; then
+  old_version=$(curl -s https://api.github.com/repos/nplh/nplh/releases/latest | jq -r ".tag_name")
+  echo "New Version: (currently $old_version)"
+  read version
+  sed -i "s/$old_version/$version/g" *.*
 
-git commit -am "Release $version"
-git push origin master
+  git commit -am "Release $version"
+  git push origin master
 
-git tag -a $version
-git push origin $version
+  git tag -a $version
+  git push origin $version
+else
+  echo "Please fix your tests before releasing"
+fi
