e4e975923c44d44c3acaaf26ed122927d2cdf4f6
Caleb Eby <caleb.eby01@gmail.com>
2017-07-24 19:20:10 -0700

    Update site

--- release.sh
+++ release.sh
@@ -1,8 +1,7 @@
 old_version=$(curl -s https://api.github.com/repos/nplh/nplh/releases/latest | jq -r ".tag_name")
 echo "New Version: (currently $old_version)"
 read version
-sed -i "s/$old_version/$version/g" nplh.go
-sed -i "s/$old_version/$version/g" install.sh
+sed -i "s/$old_version/$version/g" *
 
 git commit -am "Release $version"
 git push origin master
--- site.sh
+++ site.sh
@@ -1,41 +1,17 @@
 #!/usr/bin/env sh
 
+# clear build dir
 rm -rf build
 mkdir build
+
+version=0.0.30
 
 redirects=""
 
 redirect() {
-  redirects=$redirects$1"   "$2$'\n'
+  echo "$redirects$1  $2" >> build/_redirects
 }
 
-latest=$(git describe --abbrev=0)
+cp install.sh  build/dl
 
-pwd
-ls
-git pull
-git describe
-
-echo latest
-echo "$latest"
-
-# latest_url="$(curl \
-#   https://gitlab.com/api/v3/projects/nplh%2Fnplh/repository/tags/$latest | \
-#   jq -r ".release.description")"
-
-latest_url="$(curl \
-  https://gitlab.com/api/v3/projects/nplh%2Fnplh/repository/tags/$latest | \
-  sed -r 's/\{|,/\n/g' | \
-  grep description | \
-  sed -r 's/:/\n/' | \
-  tail -n 1 | \
-  cut -d \" -f2
-)"
-
-echo latest url
-echo "$latest_url"
-
-redirect /dl "$latest_url"
-
-echo "$redirects"
-echo "$redirects" > build/_redirects
+redirect / https://github.com/nplh/nplh
